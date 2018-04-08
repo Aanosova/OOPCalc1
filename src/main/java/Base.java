0fb368b4c8852aef3ae616ba41c19677a1f47f55
Aanosova <anosova.aleksandra@gmail.com>
2018-04-08 21:36:19 +0300

    поправки

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -7,17 +7,16 @@ public class Base {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        /**try {*/
-            System.out.println("Введите первое число: ");
+            System.out.println("Введите первое дробное число: ");
             double a = scanner.nextDouble();
 
-            System.out.println("Введите второе число: ");
+            System.out.println("Введите второе дробное число: ");
             double b = scanner.nextDouble();
 
             System.out.println("Введите операцию (+,-,*,/): ");
             String oper = scanner.next();
 
-            System.out.println("Ответ:");
+            System.out.println("Результат:");
             Math calc = new Math();
 
             System.out.println(calc.calculate(a, b, oper));
